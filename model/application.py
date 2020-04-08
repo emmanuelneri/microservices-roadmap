fba4d184f7b55715ac4f30459c1c76219dc89fde
emmanuelneri <emmanuelnerisouza@gmail.com>
2020-04-07 23:55:34 -0300

    add nodes

--- model/application.py
+++ model/application.py
@@ -6,10 +6,20 @@ class Application:
     def build(self, dot):
         dot.node('application', 'Application')        
 
-        dot.edge('application', 'db') 
         dot.edge('application', 'stack')
+        dot.edge('application', 'db')
+        dot.edge('application', 'data-management')  
         dot.edge('application', 'monitoring')
         dot.edge('application', 'comunication')
+        dot.edge('application', 'consistency')
+
+        dot.node('stack', 'Stack')
+        dot.node('linguagens', 'Linguagens')
+        dot.node('framework', 'Frameworks')
+        dot.node('lib', 'Libs')
+        dot.edge('stack', 'linguagens')
+        dot.edge('stack', 'framework')
+        dot.edge('stack', 'lib')
 
         dot.node('db', 'Database')
         dot.node('sql', 'SQL')
@@ -19,13 +29,11 @@ class Application:
         dot.edge('db', 'nosql')
         dot.edge('db', 'cache')
 
-        dot.node('stack', 'Stack')
-        dot.node('linguagens', 'Linguagens')
-        dot.node('framework', 'Frameworks')
-        dot.node('lib', 'Libs')
-        dot.edge('stack', 'linguagens')
-        dot.edge('stack', 'framework')
-        dot.edge('stack', 'lib')
+        dot.node('data-management', 'Datamanagement')
+        dot.node('cqrs', 'CQRS')
+        dot.node('event-sourcing', 'Event Sourcing')
+        dot.edge('data-management', 'cqrs')
+        dot.edge('data-management', 'event-sourcing')
 
         dot.node('monitoring', 'Monitoring')
         dot.node('health-check', 'Health Check')
@@ -38,7 +46,7 @@ class Application:
         dot.node('synchronous', 'Synchronous')
         dot.node('asynchronous', 'Asynchronous')
         dot.edge('comunication', 'synchronous')
-        dot.edge('comunication', 'asynchronous')
+        dot.edge('comunication', 'asynchronous')        
 
         dot.node('grpc', 'gRPC')
         dot.node('http', 'HTTP')
@@ -60,15 +68,24 @@ class Application:
 
         dot.node('idempotent', 'Idempotent Consumer')
         dot.node('deduplicate', 'Deduplicate-message')
-        dot.node('eventual-consistency', 'eventual consistency')
+        dot.node('tolerant-reader', 'tolerant-reader')
         dot.edge('pub-sub', 'idempotent')
         dot.edge('pub-sub', 'deduplicate')
-        dot.edge('pub-sub', 'eventual-consistency')
+        dot.edge('pub-sub', 'tolerant-reader')
 
         dot.edge('application', 'fault-tolerance')
         dot.node('fault-tolerance', 'Fault Tolerance')
         dot.node('app-circuit-breaker', 'Circuit Breaker')
         dot.node('dlq', 'Dead Letter Queues')
+        dot.node('retry', 'Retry')
+
+        dot.node('consistency', 'Consistency')
+        dot.node('eventual-consistency', 'eventual consistency')
+        dot.node('saga', 'SAGA')
+        dot.edge('consistency', 'eventual-consistency')
+        dot.edge('consistency', 'saga')
+
         dot.edge('fault-tolerance', 'app-circuit-breaker')
         dot.edge('fault-tolerance', 'dlq') 
+        dot.edge('fault-tolerance', 'retry') 
     
--- model/process.py
+++ model/process.py
@@ -8,4 +8,19 @@ class Process:
 
         dot.edge('process', 'continuous-integration')
         dot.edge('process', 'continuous-delivery')
+        dot.edge('process', 'deploy')
+
+
+        dot.node('deploy', 'deploy')
+
+        dot.node('blue-green-deployment', 'Blue/Green Deployment')
+        dot.node('canary-release', 'Canary release')
+
+        dot.edge('deploy', 'blue-green-deployment')
+        dot.edge('deploy', 'canary-release')
+
+
+
+
+
     
